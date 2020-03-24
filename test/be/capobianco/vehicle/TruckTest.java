1daaf8d6bec4d0bad08a6770c2905de6fd32e238
ViralTaco <7462994+viraltaco@users.noreply.github.com>
2020-03-24 19:40:21 +0100

    Updated tests

--- test/be/capobianco/vehicle/TruckTest.java
+++ test/be/capobianco/vehicle/TruckTest.java
@@ -4,51 +4,57 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import be.capobianco.vehicle.exception.TruckException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class TruckTest {
-
   private Truck actual;
-
-  @BeforeEach
-  void setUp() {
-    this.actual = new Truck();
-  }
 
   @Test
   void testGetAndSetSlotsWithValidValue() {
+    assertDoesNotThrow(() -> this.actual = new Truck(Truck.minSlots));
+    assertEquals(Truck.minSlots, actual.getSlots());
     assertDoesNotThrow(() -> this.actual.setSlots(Truck.maxSlots));
     assertEquals(Truck.maxSlots, this.actual.getSlots());
   }
 
   @Test
-  void testSetSlotsThrowsWhenValueBiggerThanMax() {
-    assertThrows(IllegalArgumentException.class,
+  void testSetSlotsThrowsWhenValueIsNotInRange() {
+    // above
+    assertDoesNotThrow(() -> this.actual = new Truck(3.0));
+    assertThrows(TruckException.class,
         () -> this.actual.setSlots(Truck.maxSlots + 1.0)
     );
-  }
-
-  @Test
-  void testSetSlotsThrowsWhenValueSmallerThanMin() {
-    assertThrows(IllegalArgumentException.class,
+    // below
+    assertDoesNotThrow(() -> this.actual = new Truck(3.0));
+    assertThrows(TruckException.class,
         () -> this.actual.setSlots(Truck.minSlots - 1.0)
+    );
+    // nan
+    assertThrows(TruckException.class,
+                 () -> this.actual = new Truck(Double.NaN));
+    assertThrows(TruckException.class,
+        () -> this.actual.setSlots(Double.NaN)
     );
   }
 
   @Test
   void testNewTruckWithInvalidValue() {
-    assertThrows(IllegalArgumentException.class,
-        () -> this.actual = new Truck(10.0),
-        "Truck takes from 3 to 5 Vehicle slots");
+    assertThrows(TruckException.class,
+        () -> this.actual = new Truck(Truck.minSlots - 1.0));
+    assertThrows(TruckException.class,
+        () -> this.actual = new Truck(Truck.maxSlots + 1.0));
   }
 
   @Test
   void testSetterWithValidValues() {
+    assertDoesNotThrow(() -> this.actual = new Truck(3.0));
     this.actual.setPassengers(10);
-    assertDoesNotThrow(() -> actual.setSlots(3.0));
-
-    assertEquals(3.0, this.actual.getSlots());
     assertEquals(10, this.actual.getPassengers());
+
+    assertDoesNotThrow(() -> this.actual.setSlots(3.0));
+    assertEquals(3.0, this.actual.getSlots());
+
   }
 }
