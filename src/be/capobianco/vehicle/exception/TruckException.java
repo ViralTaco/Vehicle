7c951b3f7c758085a0c4f0187eced5d7f7b70e58
ViralTaco <7462994+viraltaco@users.noreply.github.com>
2020-03-24 19:56:13 +0100

    adjusted string format for the change to Truck.minSlots and Truck.maxSlots change in type (int -> double)

--- src/be/capobianco/vehicle/exception/TruckException.java
+++ src/be/capobianco/vehicle/exception/TruckException.java
@@ -3,8 +3,8 @@ package be.capobianco.vehicle.exception;
 import be.capobianco.vehicle.Truck;
 
 public class TruckException extends RuntimeException {
-    private static final String slotRange =
-        String.format("Truck expects a number of slots between %d and %d.",
+    private static final String slotRange = /// %.0f <- don't show decimals.
+        String.format("Truck expects a number of slots between %.0f and %.0f.",
                       Truck.minSlots,
                       Truck.maxSlots);
 
